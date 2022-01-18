e62ba3438012121b78d95e50d3f904e45d8fb63f
unknown <jajonsimbeta>
2022-01-18 10:35:10 +0900

    수정

--- js/eduon_ui.js
+++ js/eduon_ui.js
@@ -3,8 +3,10 @@ $(document).ready(function(){
    
     $('.eduon_header').hover(function(){
         $('.eduon_header').addClass('header_over');
+        $('.eduon_wrap').addClass('gnbmask');// 마우스 오버시 마스크 켜기
     },function(){
         $('.eduon_header').removeClass('header_over');
+        $('.eduon_wrap').removeClass('gnbmask');
     });
    
 
